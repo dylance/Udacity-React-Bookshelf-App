b0b72fa19ff4f5c7d51712bcf98f88e3535d270c
Dylan Ellison <dylancellison@gmail.com>
2019-01-14 17:41:58 -0600

    feat: Add books to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import * as BooksAPI from "../BooksAPI";
 
 class App extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({
+        books
+      });
+    });
+  }
+
   render() {
     return <div>hey there</div>;
-  };
-};
+  }
+}
 
 export default App;
