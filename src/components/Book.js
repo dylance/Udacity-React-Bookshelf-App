49fa8ce3c825efb875a11ff72ffe94702b3b1e77
Dylan Ellison <dylancellison@gmail.com>
2019-01-14 18:38:27 -0600

    feat: Book's shelf is set as default option in select element

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,7 +7,7 @@ const Book = props => {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select value={props.book.shelf}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
