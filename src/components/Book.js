aec727112ad535f940b2f2783ea36e830927f299
Dylan Ellison <dylancellison@gmail.com>
2019-01-16 06:00:06 -0600

    fix: changed proptype of book from object to array in Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 class Book extends Component {
   static propTypes = {
-  book: PropTypes.array.isRequired,
+  book: PropTypes.object.isRequired,
   usersBooks: PropTypes.array,
   changeShelf: PropTypes.func.isRequired
 };
